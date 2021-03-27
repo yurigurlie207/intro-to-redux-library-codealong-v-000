b1d6e97dbb8d6e0c372e560ea38bdadb6507f6b8
yurigurlie207 <adrienne.yao@gmail.com>
2021-03-26 22:39:09 -0400

    commit tree e7927ec348ec4da17dd57f0142ff43730f455657

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,16 +1,22 @@
 export default function shoppingListItemReducer(
 	state = {
-		items: []
+	  items: []
 	},
 	action
-) {
-	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return {
-				...state,
-				items: state.items.concat(state.items.length + 1)
-			}
-		default:
-			return state;
+  ) {
+	console.log(action);
+	switch(action.type) {
+  
+	  case 'INCREASE_COUNT':
+		console.log("Current state.items length %s", state.items.length);
+		console.log("Updating state.items length to %s", state.items.length + 1);
+		return {
+		  ...state,
+		  items: state.items.concat(state.items.length + 1)
+		}
+  
+	  default:
+		console.log('Initial state.items length: %s', state.items.length);
+		return state;
 	}
-}
+  }
